2f3b97234993cea2a1a445fdcd4a39000d7dbcd4
tsitr <tsitritsky@gmail.com>
2019-01-28 07:51:39 +0200

    First commit

---
+++ src/main/java/com/tsitritsky/Spring5Test/Spring5TestApplication.java
@@ -0,0 +1,14 @@
+package com.tsitritsky.Spring5Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5TestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tsitritsky/Spring5Test/Spring5TestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tsitritsky.Spring5Test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5TestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
